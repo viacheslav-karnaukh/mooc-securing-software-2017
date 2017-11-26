cf06ec5326010e05b79cc686506780c296561198
Viacheslav Karnaukh <vyacheslav.karnauh@gmail.com>
2017-11-26 18:37:16 +0200

    Add Set1-04.HelloTemplates

---
+++ Set1-04.HelloTemplates/src/main/java/sec/hellotemplates/HelloTemplatesController.java
@@ -0,0 +1,17 @@
+package sec.hellotemplates;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloTemplatesController {
+    @RequestMapping("/")
+    public String home() {
+            return "index";
+    }
+
+    @RequestMapping("/video")
+    public String video() {
+            return "video";
+    }
+}
